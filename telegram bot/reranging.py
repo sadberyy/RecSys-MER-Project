53d47a54e683c836efce5470d57d4d6cfd8343b9
Daniela Pekerskaya <125833838+aellieme@users.noreply.github.com>
2025-03-22 23:10:48 +0300

    Add files via upload
    Ранжирование

---
+++ telegram bot/reranging.py
@@ -0,0 +1,20 @@
+def rearrange_tracks(user_mood, tracks_with_moods):
+    mood_values = {
+        'Anger': 0,
+        'Sad': 1,
+        'Joy': 2,
+        'Pleasure': 3
+    }
+
+    def mood_similarity(user_mood, tracks_with_moods):
+        return abs(mood_values[user_mood] - mood_values[tracks_with_moods])
+
+
+    sorted_tracks = sorted(tracks_with_moods, key=lambda track: mood_similarity(user_mood, track['mood']))
+
+    return sorted_tracks
+
+# def print_sorted(user_mood, tracks_with_moods):
+#     sorted_tracks = rearrange_tracks(user_mood, tracks_with_moods)
+#     for track in sorted_tracks:
+#         print(f'{track["track_name"]} - {track["mood"]}')

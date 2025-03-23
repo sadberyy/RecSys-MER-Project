7e1e9552cd6056c32ced171d933293cafc1fe92d
Daniela Pekerskaya <125833838+aellieme@users.noreply.github.com>
2025-03-23 11:20:24 +0300

    lastest version

--- telegram bot/recommendations.py
+++ telegram bot/recommendations.py
@@ -4,12 +4,15 @@ import pandas as pd
 import pickle
 from implicit.als import AlternatingLeastSquares
 from scipy.sparse import csr_matrix
-from TopPopular_file import load_model_toppop
-import bot
 
+def load_model_toppop(filename: str):
+    with open(filename, 'rb') as f:
+        model = pickle.load(f)
+    print(f"Model loaded from {filename}")
+    return model
 
-user_history_path = "UserListening History.csv"  
-songs_path = "Music Info.csv"  
+user_history_path = "User Listening History.csv"
+songs_path = "../../../../Downloads/Music Info.csv"
 
 
 user_history = pd.read_csv(user_history_path)
@@ -25,13 +28,14 @@ track_encoder = artifacts["track_encoder"]
 minfo = artifacts["minfo"]
 
 
-TopPopular_path = 'TopPopular_model.pkl'
-TopPopular_loaded_model = load_model_toppop(TopPopular_path)
+#TopPopular_path = 'TopPopular_model.pkl'
+#TopPopular_loaded_model = load_model_toppop(TopPopular_path)
 
 def build_user_item_matrix(user_history, user_encoder, track_encoder):
     user_history = user_history.dropna(subset=["user_id", "track_id"]) 
-    user_ids = user_encoder.transform(user_history["user_id"])
-    track_ids = track_encoder.transform(user_history["track_id"])
+    user_ids = [user_encoder.get(uid, -1) for uid in user_history["user_id"]]
+
+    track_ids = [track_encoder.get(uid, -1) for uid in user_history["track_id"]]
     data = [1] * len(user_ids) 
     user_item_matrix = csr_matrix(
         (data, (user_ids, track_ids)),
@@ -45,10 +49,11 @@ user_item_matrix = build_user_item_matrix(user_history, user_encoder, track_enco
 
 def get_als_recommendations(user_id, model, user_encoder, track_encoder, minfo, user_item_matrix, history_df, top_k = 10):
     try:
-        if user_id not in user_encoder.classes_:
+        if user_id not in user_encoder:
             print(f"Пользователь {user_id} не найден в ALS, переключаемся на TopPopular")
             return recommend_top_popular(user_id, top_k)
-        internal_id = user_encoder.transform([user_id])[0]
+        internal_id = user_encoder[user_id]
+        print(internal_id)
         if internal_id >= user_item_matrix.shape[0]:
             return pd.DataFrame(columns = ["artist", "name"])
         
@@ -88,15 +93,13 @@ def recommend_top_popular(user_id: int, topn: int = 10) -> pd.DataFrame:
     return pd.DataFrame(recommended_songs)
 
 
-def get_recommendations(user_id:int, topn:int = 10)-> pd.DataFrame:
+def get_recommendations(flags, user_data, user_id:int, topn:int = 10)-> pd.DataFrame:
     
-    user_new_or_not = bot2.flags.get(bot2.user_data, True)
+    user_new_or_not = flags.get(user_data, True)
     if user_new_or_not ==True or user_id == 0:
         return recommend_top_popular(user_id, topn)
     else:
         return get_als_recommendations(user_id, als_model, user_encoder, track_encoder, songs, user_item_matrix, user_history, top_k=topn)
     
     
-
-
 

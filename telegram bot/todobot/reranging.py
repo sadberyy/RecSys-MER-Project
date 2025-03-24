a6857b66c8fa1f45502c262d2aff1f355a51a370
Daniela Pekerskaya <elyasup@yandex.ru>
2025-03-24 23:47:46 +0300

    new reranging

--- telegram bot/todobot/reranging.py
+++ telegram bot/todobot/reranging.py
@@ -1,13 +1,26 @@
+import pandas as pd
 def rearrange_tracks(user_mood, tracks_with_moods):
-    mood_values = {
-        'Anger': 0,
-        'Sad': 1,
-        'Joy': 2,
-        'Pleasure': 3
-    }
-    mood_ids = {v: k for k, v in mood_values.items()}
 
-    tracks_with_moods["mood"] = tracks_with_moods["mood"].map(mood_values)
-    sorted_tracks = tracks_with_moods.sort_values(by = "mood", key = lambda track: (track-mood_values[user_mood]).abs())
-    sorted_tracks["mood"] = sorted_tracks["mood"].map(mood_ids)
+    mood_sort_order = {
+        'Sad': ['Sad', 'Anger', 'Pleasure', 'Joy'],
+        'Anger': ['Anger', 'Sad', 'Joy', 'Pleasure'],
+        'Joy': ['Joy', 'Pleasure', 'Anger', 'Sad'],
+        'Pleasure': ['Pleasure', 'Joy', 'Sad', 'Anger']
+    }
+
+    desired_mood_order = mood_sort_order[user_mood]
+
+    tracks_with_moods['mood'] = pd.Categorical(
+        tracks_with_moods['mood'],
+        categories=desired_mood_order,
+        ordered=True
+    )
+
+    sorted_tracks = tracks_with_moods.sort_values(by='mood')
+
     return sorted_tracks
+
+def print_sorted(user_mood, tracks_with_moods):
+    sorted_tracks = rearrange_tracks(user_mood, tracks_with_moods)
+    for track in sorted_tracks:
+        print(f'{track["track_name"]} - {track["mood"]}')
